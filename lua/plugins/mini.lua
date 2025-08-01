0a70d09701c53206c881e04061c6c6f7ecc97c3f
= <poweski2009@gmail.com>
2025-08-01 21:11:24 +0200

    feat: mini_modules->mini_dot with inline sub plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,10 +1,6 @@
 return {
    "echasnovski/mini.nvim",
    version = "*",
-   config = function ()
-      local modules = require("config.mini_modules") -- file with all configurations
-      -- enable these mini sub modules
-      modules:use("mini.pairs", "mini.surround")
-   end
+   config = function () require("config.mini_dot") end
 }
 
