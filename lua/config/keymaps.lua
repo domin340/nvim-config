861f46a5c666c587269606460852de9d31cabf3d
= <poweski2009@gmail.com>
2025-08-12 19:43:50 +0200

    feat: number line + .emmyrc + gitsigns

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 vim.opt.termguicolors = true
 
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -10,5 +10,6 @@ return {
 		map_key("n", "<leader>n", "<CMD>bnext<CR>", { desc = "go to next buffer" })
 		map_key("n", "<leader>p", "<CMD>bprev<CR>", { desc = "go to previous buffer" })
 		map_key("n", "<leader>bd", "<CMD>bdelete<CR>", { desc = "delete current buffer" })
+		map_key("n", "<leader>bp", "<CMD>BufferLineTogglePin<CR>", { desc = "pin current buffer" })
 	end,
 }
