6fc627dab9c5d25ca3c79035f3795eed89bdff84
= <poweski2009@gmail.com>
2025-09-04 22:10:41 +0200

    conclusion: I want ui-select.

--- lua/plugins/telescope_conf.lua
+++ lua/plugins/telescope_conf.lua
@@ -5,16 +5,24 @@ return {
 		tag = "0.1.8",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
 			"ahmedkhalf/project.nvim", -- with telescope extension
 		},
 		config = function()
 			require("telescope").setup()
+			require("telescope").load_extension("ui-select")
 
-			-- prerequisites
+			-- see all viewed project roots recently opened
+			require("telescope").load_extension("projects")
+			require("project_nvim").setup({
+				detection_methods = { "pattern" },
+				patterns = { ".git", "package.json" },
+			})
+
+			-- telescope key maps
 			local builtin = require("telescope.builtin")
 			local map_key = vim.keymap.set
 
-			-- basic key maps
 			map_key("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 			map_key("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 			map_key("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
@@ -24,13 +32,6 @@ return {
 				builtin.find_files({ hidden = true })
 			end, { desc = "Telescope find all files" })
 
-			-- projects to see all viewed project roots recently opened
-			require("project_nvim").setup({
-				detection_methods = { "pattern" },
-				patterns = { ".git", "package.json" },
-			})
-
-			require("telescope").load_extension("projects")
 			map_key("n", "<leader>fp", "<CMD>Telescope projects<CR>", { desc = "Telescope Projects" })
 		end,
 	},
