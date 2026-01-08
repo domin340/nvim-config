18854547c62fc9d316aae40840914514b905e263
DOMIN1310 <poweski2009@gmail.com>
2026-01-08 16:30:23 +0100

    feat: heirline

---
+++ lua/plugins/heirline.lua
@@ -0,0 +1,8 @@
+return {
+	'rebelot/heirline.nvim',
+   config = function ()
+      -- require('heirline').setup({
+      --    statusline = require('configs.status_heirline'),
+      -- })
+   end
+}
