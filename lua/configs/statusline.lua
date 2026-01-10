afd0c5e3d79d7722673c218c5f3a686012b8f2f6
DOMIN1310 <poweski2009@gmail.com>
2026-01-10 23:17:33 +0100

    feat: lsp comp. changes + ActiveCounter

--- lua/configs/statusline.lua
+++ lua/configs/statusline.lua
@@ -2,16 +2,18 @@ local h = require 'core.heirline-comps.h'
 local Spaced = h.Spaced
 local WriteEnd = h.WriteEnd
 
+local lsps = require 'core.heirline-comps.lsp'
+local LspsCounterBox = lsps.CounterBox
+
 local BufNote = require 'core.heirline-comps.buf-note'
 local GitBranch = require('core.heirline-comps.git').Branch
-local LspsBox = require('core.heirline-comps.cur-lsps').LspsBox
 local Cursor = require('core.heirline-comps.cursor').Cursor
 
 local FrontComponents = {}
 
 local BackComponents = Spaced {
 	GitBranch,
-	LspsBox,
+	LspsCounterBox,
 	BufNote,
 	Cursor,
 }
---
+++ lua/core/heirline-comps/lsp.lua
@@ -0,0 +1,78 @@
+local h = require 'core.heirline-comps.h'
+local Box = h.Box
+
+---pasting it to update properties makes the component update every possible time
+---lsp counter for buffer could possibly change.
+local clients_update = { 'LspAttach', 'LspDetach', 'BufEnter', 'BufLeave' }
+
+---@class heirline-comps.lsps-list
+---@field is_any_active fun(self: heirline-comps.lsps-list): boolean
+---@field clients vim.lsp.Client[]
+local List = {
+	static = {
+		---@param self heirline-comps.lsps-list
+		is_any_active = function(self)
+			return #self.clients == 0 or not vim.diagnostic.is_enabled()
+		end,
+	},
+
+	---@param self heirline-comps.lsps-list
+	init = function(self)
+		self.clients = vim.lsp.get_clients { bufnr = 0 }
+	end,
+
+	update = clients_update,
+
+	---@param self heirline-comps.lsps-list
+	provider = function(self)
+		if self:is_any_active() then
+			return '@none'
+		end
+
+		local lsps_str, concat_str = '', ' '
+		for i, value in ipairs(self.clients) do
+			lsps_str = lsps_str .. value.name
+			if #self.clients > i then
+				lsps_str = lsps_str .. concat_str
+			end
+		end
+
+		return lsps_str
+	end,
+}
+
+---@class heirline-comps.is-active
+---@field clients vim.lsp.Client[]
+local ActiveCounter = {
+	---@param self heirline-comps.is-active
+	init = function(self)
+		self.clients = vim.lsp.get_clients { bufnr = 0 }
+	end,
+
+	update = clients_update,
+
+	---@param self heirline-comps.is-active
+	provider = function(self)
+		local count = #self.clients
+
+		if count == 0 then
+			return 'no clients'
+		elseif count == 1 then
+			return '1 client'
+		else
+			return count .. ' clients'
+		end
+	end,
+}
+
+-- colors used for boxes
+local bg_clr, text_clr = 'darker_blue', 'white'
+local CounterBox = Box({ bg_clr = 'darker_blue', text_clr = 'white' }, ActiveCounter)
+local LspsBox = Box({ bg_clr = bg_clr, text_clr = text_clr }, List)
+
+return {
+	List = List,
+	ActiveCounter = ActiveCounter,
+	CounterBox = CounterBox,
+	LspsBox = LspsBox,
+}
