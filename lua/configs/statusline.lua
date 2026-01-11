9a47ca390d668a578d5db3e7e44b94e906c56a6c
DOMIN1310 <poweski2009@gmail.com>
2026-01-11 12:01:18 +0100

    feat: separated relpath-file into multiple

--- lua/configs/statusline.lua
+++ lua/configs/statusline.lua
@@ -2,15 +2,24 @@ local h = require 'core.heirline-comps.h'
 local WriteEnd = h.WriteEnd
 local Spaced = h.Spaced
 
--- local GitBranch = require 'core.heirline-comps.git-branch'
+local FstatusIcon = require 'core.heirline-comps.fstatus-icon'
 local RelpathFile = require 'core.heirline-comps.relpath-file'
+local FileModified = require 'core.heirline-comps.file-modified'
+
+local Relpath = {
+	FstatusIcon,
+	RelpathFile,
+	FileModified,
+}
+
+-- local GitBranch = require 'core.heirline-comps.git-branch'
 -- local GitChanges = require 'core.heirline-comps.git-changes'
 -- local Diagnostics = require 'core.heirline-comps.diagnostics'
 -- local LspMessage = require 'core.heirline-comps.lsp-list'
 local Cursor = require 'core.heirline-comps.cursor'
 
 local FrontComponents = {
-	RelpathFile,
+	Relpath,
 }
 
 local BackComponents = Spaced {
--- lua/core/heirline-comps/relpath-file.lua
+++ lua/core/heirline-comps/relpath-file.lua
@@ -1,74 +1,24 @@
----@class heirline-comps.relpath-file-icon : heirline-comps.relpaht-file-display
-local FileIcon = {
-	{
-		---@param self heirline-comps.relpath-file-icon
-		condition = function(self)
-			return not self.readonly
-		end,
-
-		hl = { fg = 'sym' },
-
-		---@param self heirline-comps.relpath-file-icon
-		provider = function(self)
-			return self.file and '󰈔 ' or ' '
-		end,
-	},
-	{
-		---@param self heirline-comps.relpath-file-icon
-		condition = function(self)
-			return self.readonly
-		end,
-
-		hl = { fg = 'kw' },
-
-		---@param self heirline-comps.relpath-file-icon
-		provider = function(self)
-			return self.file and '󰈡 ' or '󰉐 '
-		end,
-	},
-}
-
 ---@class heirline-comps.relpaht-file-display
 ---@field filepath string file path of the current buffer
----@field file boolean whether filepath is a readable file
----@field readonly boolean
-local RelpathFileDisplay = {
+local RelpathFile = {
 	update = { 'BufEnter', 'BufLeave' },
 
 	---@param self heirline-comps.relpaht-file-display
 	init = function(self)
 		self.filepath = vim.api.nvim_buf_get_name(0)
-		self.file = vim.fn.filereadable(self.filepath) == 1 and true or false
-		self.readonly = vim.bo.readonly or not vim.bo.modifiable
 	end,
 
-	FileIcon,
-	{
-		---@param self heirline-comps.relpaht-file-display
-		provider = function(self)
-			local cwd = vim.fn.getcwd()
-			local relpath = vim.fs.relpath(cwd, self.filepath)
+	---@param self heirline-comps.relpaht-file-display
+	provider = function(self)
+		local cwd = vim.fn.getcwd()
+		local relpath = vim.fs.relpath(cwd, self.filepath)
 
-			if relpath == '.' then
-				return './'
-			else
-				return relpath
-			end
-		end,
-	},
-}
-
-local FileModifiedFlag = {
-	condition = function()
-		return vim.bo.modified
+		if relpath == '.' then
+			return './'
+		else
+			return relpath
+		end
 	end,
-
-	hl = { fg = 'str' },
-
-	provider = '[+]',
 }
 
-return {
-   RelpathFileDisplay,
-   FileModifiedFlag,
-}
+return RelpathFile
