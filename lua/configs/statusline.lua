70e507b97b6d21f806895675f46ba5d73b2c903e
DOMIN1310 <poweski2009@gmail.com>
2026-01-11 11:43:34 +0100

    feat: ff

--- lua/configs/statusline.lua
+++ lua/configs/statusline.lua
@@ -4,7 +4,8 @@ local Spaced = h.Spaced
 -- local GitBranch = require 'core.heirline-comps.git-branch'
 local RelpathFile = require 'core.heirline-comps.relpath-file'
 -- local GitChanges = require 'core.heirline-comps.git-changes'
--- local LspList = require 'core.heirline-comps.lsp-list'
+-- local Diagnostics = require 'core.heirline-comps.diagnostics'
+-- local LspMessage = require 'core.heirline-comps.lsp-list'
 local Cursor = require 'core.heirline-comps.cursor'
 
 local FrontComponents = {
