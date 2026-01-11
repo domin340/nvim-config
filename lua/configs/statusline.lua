cf373429f15b078739a031637035f0c31c7a4391
DOMIN1310 <poweski2009@gmail.com>
2026-01-11 10:13:18 +0100

    removed experimental, template

--- lua/configs/statusline.lua
+++ lua/configs/statusline.lua
@@ -1,21 +1,23 @@
-local c = require 'core.heirline-comps'
+local h = require 'core.heirline-comps.h'
 
-local LspCounterBox = c.Box({ bg_clr = 'darker_blue', text_clr = 'white' }, c.LspActiveCounter)
-local BufBox = c.Box({ bg_clr = 'gray', text_clr = 'white' }, c.IconBufNr)
+-- local GitBranch = require 'core.heirline-comps.git-branch'
+-- local RelpathFile = require 'core.heirline-comps.relpath-file'
+-- local FileMod = require 'core.heirline-comps.file-mod'
+-- local GitChanges = require 'core.heirline-comps.git-changes'
+-- local LspList = require 'core.heirline-comps.lsp-list'
+-- local Cursor = require 'core.heirline-comps.cursor'
 
 local FrontComponents = {}
 
-local BackComponents = c.Spaced {
-	c.GitBranch,
-	LspCounterBox,
-	BufBox,
-	c.Cursor,
-}
+local BackComponents = h.Spaced {}
 
 return {
 	hl = { bg = 'nbg' },
 
 	FrontComponents,
-	c.WriteEnd,
+	h.WriteEnd,
 	BackComponents,
 }
+
+-- [Branch] [File] [Dirty/Mod] [Changes] [%=] [LspList] [Cursor]
+
