0609732dad06f948e34662e6f9d546308a88dda1
DOMIN1310 <poweski2009@gmail.com>
2026-01-11 17:53:31 +0100

    feat: git changes + colors fix

--- lua/configs/statusline.lua
+++ lua/configs/statusline.lua
@@ -1,9 +1,10 @@
 local h = require 'core.heirline-comps.h'
-local WriteEnd, Space, Spaced = h.WriteEnd, h.Space, h.Spaced
+local WriteEnd, Spaced = h.WriteEnd, h.Spaced
 
 -- local LspDiagnostics = require 'core.heirline-comps.diagnostics'
 -- local LspList = require 'core.heirline-comps.lsp-list'
 local GitBranchBox = require 'core.heirline-comps.git-branch-box'
+local GitChanges = require 'core.heirline-comps.git-changes'
 local Cursor = require 'core.heirline-comps.cursor'
 local RelpathFile = require 'core.heirline-comps.relpath-file'
 local FileModified = require 'core.heirline-comps.file-modified'
@@ -12,13 +13,13 @@ local FileModified = require 'core.heirline-comps.file-modified'
 local Relpath = {
 	-- FstatusIcon,
 	RelpathFile,
-	Space,
 	FileModified,
 }
 
 local Left = Spaced {
 	GitBranchBox,
 	Relpath,
+	GitChanges,
 }
 
 local Right = Spaced {
