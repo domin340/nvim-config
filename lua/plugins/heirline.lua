8503c01e7b6b8086859d0ba7198f0fffe1f8ca78
domin340 <poweski2009@gmail.com>
2026-06-06 11:18:28 +0200

    feat: hide statusline

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -5,23 +5,23 @@ return {
 		'nvim-tree/nvim-web-devicons',
 	},
 	config = function()
-		local utils = require 'heirline.utils'
+		-- local utils = require 'heirline.utils'
+		--
+		-- local custom_heirline = require 'config.custom-heirline'
+		-- local setup_colors = custom_heirline.setup_colors
+		--
+		-- -- set up colors before everything
+		-- require('heirline').load_colors(setup_colors)
+		-- vim.api.nvim_create_augroup('Heirline', { clear = true })
+		-- vim.api.nvim_create_autocmd('ColorScheme', {
+		-- 	callback = function()
+		-- 		utils.on_colorscheme(setup_colors)
+		-- 	end,
+		-- 	group = 'Heirline',
+		-- })
 
-		local custom_heirline = require 'config.custom-heirline'
-		local setup_colors = custom_heirline.setup_colors
-
-		-- set up colors before everything
-		require('heirline').load_colors(setup_colors)
-		vim.api.nvim_create_augroup('Heirline', { clear = true })
-		vim.api.nvim_create_autocmd('ColorScheme', {
-			callback = function()
-				utils.on_colorscheme(setup_colors)
-			end,
-			group = 'Heirline',
-		})
-
-		require('heirline').setup {
-			statusline = custom_heirline.statusline,
-		}
+		-- require('heirline').setup {
+		-- statusline = custom_heirline.statusline,
+		-- }
 	end,
 }
