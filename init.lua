413e64efcb42096f71c0b39b4073f758e8a2c7da
domin340 <poweski2009@gmail.com>
2026-06-20 21:05:52 +0200

    feat: show default statusline

--- init.lua
+++ init.lua
@@ -5,5 +5,4 @@ require 'config.lazy'
 -- comment the line below while using themehub
 -- vim.cmd.colorscheme 'catppuccin-mocha'
 -- vim.cmd.colorscheme 'jellybeans-hc'
-vim.opt.laststatus = 0
 vim.cmd.colorscheme 'wisteria'
